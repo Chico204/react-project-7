8b500975b5f8a61e0d0702b0f7ed644347bd58d1
Chico204 <olatunjimark391@gmail.com>
2025-05-24 02:56:50 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import { testimonials } from "./testimonials";
+
+export default function App() {
+ return (
+  <div>hiiii</div>
+ )
+}
---
+++ src/testimonials.js
@@ -0,0 +1,32 @@
+// testimonials.js
+
+export const testimonials = [
+  {
+    name: "Maria Ferguson",
+    title: "Office Manager",
+    quote:
+      "Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.",
+    image: 'https://www.course-api.com/images/people/person-1.jpeg',
+  },
+  {
+    name: "John Doe",
+    title: "Product Designer",
+    quote:
+      "I'm baby wolf pickled schlitz try-hard normcore marfa man bun mumblecore vice pop-up XOXO.",
+    image:'https://www.course-api.com/images/people/person-4.jpeg',
+  },
+  {
+    name: "Peter Smith",
+    title: "Frontend Developer",
+    quote:
+      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen.",
+    image: 'https://www.course-api.com/images/people/person-3.jpeg',
+  },
+   {
+    name: "Susan Anderson",
+    title: "The Boss",
+    quote:
+      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen.",
+    image: 'https://www.course-api.com/images/people/person-2.jpeg',
+  }
+];
